0418ab04661f931a5b7846d9f1e71ef5a29e276b
tanweiwei <tanweiwei@zenmen.com>
2016-10-21 19:00:45 +0800

    change the recyclerview divider decoration style.

--- app/src/main/java/com/vivifram/second/hitalk/ui/recycleview/address/DividerDecoration.java
+++ app/src/main/java/com/vivifram/second/hitalk/ui/recycleview/address/DividerDecoration.java
@@ -3,16 +3,27 @@ package com.vivifram.second.hitalk.ui.recycleview.address;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.PorterDuff;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+
+import com.avos.avoscloud.LogUtil;
+import com.vivifram.second.hitalk.R;
+import com.zuowei.utils.common.DisplayUtil;
+import com.zuowei.utils.common.NLog;
+import com.zuowei.utils.common.TagUtil;
 
 
 public class DividerDecoration extends RecyclerView.ItemDecoration {
-
-  private static final int LINE_OFFSET = 80;
 
   private static final int[] ATTRS = new int[]{
       android.R.attr.listDivider
@@ -22,12 +33,13 @@ public class DividerDecoration extends RecyclerView.ItemDecoration {
 
   public static final int VERTICAL_LIST = LinearLayoutManager.VERTICAL;
 
-  private Drawable mDivider;
-
+  private Divider mDivider;
   public DividerDecoration(Context context) {
-    final TypedArray a = context.obtainStyledAttributes(ATTRS);
+    /*final TypedArray a = context.obtainStyledAttributes(ATTRS);
     mDivider = a.getDrawable(0);
-    a.recycle();
+    a.recycle();*/
+    mDivider = new Divider(context.getResources().getColor(R.color.color_eeeeee),
+            context.getResources().getColor(R.color.colorWhite));
   }
 
   private int getOrientation(RecyclerView parent) {
@@ -64,7 +76,7 @@ public class DividerDecoration extends RecyclerView.ItemDecoration {
           .getLayoutParams();
       final int top = Math.max(recyclerViewTop, child.getBottom() + params.bottomMargin);
       final int bottom = Math.min(recyclerViewBottom, top + mDivider.getIntrinsicHeight());
-      mDivider.setBounds(left + LINE_OFFSET, top, right, bottom);
+      mDivider.setBounds(left, top, right, bottom);
       mDivider.draw(c);
     }
   }
@@ -80,7 +92,7 @@ public class DividerDecoration extends RecyclerView.ItemDecoration {
       final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child
           .getLayoutParams();
       final int left = Math.max(recyclerViewLeft, child.getRight() + params.rightMargin);
-      final int right = Math.min(recyclerViewRight, left + mDivider.getIntrinsicHeight());
+      final int right = Math.min(recyclerViewRight, left + mDivider.getIntrinsicWidth());
       mDivider.setBounds(left, top, right, bottom);
       mDivider.draw(c);
     }
@@ -93,6 +105,72 @@ public class DividerDecoration extends RecyclerView.ItemDecoration {
       outRect.set(0, 0, 0, mDivider.getIntrinsicHeight());
     } else {
       outRect.set(0, 0, mDivider.getIntrinsicWidth(), 0);
+    }
+  }
+
+  class Divider extends Drawable{
+
+    Paint paint;
+    int lineColor;
+    int lineBg;
+    boolean vertical;
+    int offset;
+    public Divider(int lineColor,int lineBg){
+      paint = new Paint();
+      paint.setAntiAlias(true);
+      paint.setStrokeWidth(DisplayUtil.dip2px(0.5f));
+
+      offset = DisplayUtil.dip2px(10);
+      this.lineColor = lineColor;
+      this.lineBg = lineBg;
+    }
+
+    @Override
+    public void setBounds(int left, int top, int right, int bottom) {
+      super.setBounds(left, top, right, bottom);
+      vertical = right - left > bottom - top;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+      Rect bounds = getBounds();
+      //paint.setColor(lineBg);
+      //canvas.drawLine(bounds.left,bounds.top,bounds.right,bounds.bottom,paint);
+
+      if (vertical){
+        paint.setColor(lineBg);
+        canvas.drawLine(bounds.left ,bounds.top ,bounds.left + offset ,bounds.bottom,paint);
+        paint.setColor(lineColor);
+        canvas.drawLine(bounds.left + offset,bounds.top,bounds.right,bounds.bottom,paint);
+      }else {
+        paint.setColor(lineColor);
+        canvas.drawLine(bounds.left,bounds.top,bounds.right,bounds.bottom,paint);
+      }
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter colorFilter) {
+
+    }
+
+    @Override
+    public int getOpacity() {
+      return PixelFormat.TRANSLUCENT;
+    }
+
+    @Override
+    public int getIntrinsicHeight() {
+      return DisplayUtil.dip2px(0.5f);
+    }
+
+    @Override
+    public int getIntrinsicWidth() {
+      return DisplayUtil.dip2px(0.5f);
     }
   }
 }
--- app/src/main/java/com/zuowei/utils/common/DisplayUtil.java
+++ app/src/main/java/com/zuowei/utils/common/DisplayUtil.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.util.DisplayMetrics;
 import android.view.WindowManager;
 
+import com.vivifram.second.hitalk.HiTalkApplication;
+
 public class DisplayUtil {
     /**
      * 将px值转换为dip或dp值，保证尺寸大小不变
@@ -26,6 +28,11 @@ public class DisplayUtil {
      */
     public static int dip2px(Context context, float dipValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+
+    public static int dip2px(float dipValue) {
+        final float scale = HiTalkApplication.mAppContext.getResources().getDisplayMetrics().density;
         return (int) (dipValue * scale + 0.5f);
     }
 
